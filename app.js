2e173b4f013cc22de667eb59b4f2b4aeeb752fa6
Nakul Havelia <nakulhavelia@gmail.com>
2023-05-11 22:56:26 +0530

    refactored app.js for readability

--- app.js
+++ app.js
@@ -1,101 +1,109 @@
 const express = require('express');
-var cors = require('cors');
-const app = express();
-const port = 3000;
-let morgan = require('morgan');
-let bodyParser = require('body-parser');
-let mysql = require('mysql');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 const { stringify, parse } = require('wkt');
-const { response } = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 const connection = mysql.createConnection({
-    host: '',
-    user: 'admin',
-    password: "",
-    database: "droneManager"
-    });
+  host: '',
+  user: 'admin',
+  password: '',
+  database: 'droneManager',
+});
 
+// Middleware
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-})); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+// Routes
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.post('/addFlightInstance', (req, res) => {
-    wktPointsArr = []
-    req.body.destinations.forEach((coordinate) => {
-        wktPointsArr.push(stringify({
-            type: "Point",
-            coordinates: [coordinate.lng, coordinate.lat]
-        }));
-    }); 
-    wktPointStr = wktPointsArr.toString().replaceAll(",", "\",\"");
-    wktPointStr = "\"" + wktPointStr + "\"";
-    //console.log(wktPointStr);
-    wktHub = stringify({
-        type: "Point",
-        coordinates: [req.body.hub.lng, req.body.hub.lat]
-    })
-    connection.query(`ECHO droneManager.createFlightInstance([${wktPointStr}] :> ARRAY(GEOGRAPHYPOINT), ?, ?)`, [wktHub, req.body.droneCount], (error, results, fields) => {
-        console.log(error);
-        console.log(results);
-        console.log(fields);
-        let response = {
-          id: results[0].flightInstanceId,
-          paths: []
-        };
-        results.forEach((result) => {
-          response.paths.push({
-            path: parse(result.path),
-            id: result.id
-          });
-        })
-        res.send(response);
-    });
+  const wktPointsArr = req.body.destinations.map((coordinate) => stringify({
+    type: 'Point',
+    coordinates: [coordinate.lng, coordinate.lat],
+  }));
+
+  const wktPointStr = `"${wktPointsArr.join('","')}"`;
+
+  const wktHub = stringify({
+    type: 'Point',
+    coordinates: [req.body.hub.lng, req.body.hub.lat],
   });
+
+  connection.query(
+    'CALL droneManager.createFlightInstance(?, ?, ?)',
+    [wktHub, req.body.droneCount, wktPointStr],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+
+      const response = {
+        id: results[0][0].flightInstanceId,
+        paths: results[1].map((result) => ({
+          path: parse(result.path),
+          id: result.id,
+        })),
+      };
+
+      res.json(response);
+    }
+  );
+});
 
 app.post('/killDrone', (req, res) => {
-  let wktKilledDronePoints = [];
-  console.log(req.body);
-  req.body.killedDronePoints.forEach((point) => {
-    wktKilledDronePoints.push(stringify({
-      type: "Point",
-      coordinates: [point.lng, point.lat]
-    }));
-  });
-  wktPointStr = wktKilledDronePoints.toString().replaceAll(",", "\",\"");
-  wktPointStr = "\"" + wktPointStr + "\"";
-  connection.query(`ECHO droneManager.killDrone(?, [${wktPointStr}] :> ARRAY(GEOGRAPHYPOINT))`, [req.body.killedDroneId, wktKilledDronePoints], (error, results, fields) => {
-    console.log(error);
-    console.log(results);
-    console.log(fields);
-    let response = {
-      id: results[0].flightInstanceId,
-      paths: []
-    };
-    results.forEach((result) => {
-      response.paths.push({
-        path: parse(result.path),
-        id: result.id
-      });
+  const wktKilledDronePoints = req.body.killedDronePoints.map((point) =>
+    stringify({
+      type: 'Point',
+      coordinates: [point.lng, point.lat],
     })
-    res.send(response);
-  })
-})
+  );
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
-connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
+  const wktPointStr = `"${wktKilledDronePoints.join('","')}"`;
+
+  connection.query(
+    'CALL droneManager.killDrone(?, ?)',
+    [req.body.killedDroneId, wktPointStr],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+
+      const response = {
+        id: results[0][0].flightInstanceId,
+        paths: results[1].map((result) => ({
+          path: parse(result.path),
+          id: result.id,
+        })),
+      };
+
+      res.json(response);
     }
-   
-    console.log('connected as id ' + connection.threadId);
+  );
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:', err.stack);
+    return;
+  }
+
+  console.log('Database connection successful');
 });
